9711ddf54717587527cf6f404ed63e463d0a4875
CRUZRO <cruz17.romina@gmail.com>
2022-04-27 22:02:18 -0400

    added link for logo and edited work link

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ console.log('Welcome to my portfolio!')
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
-      x.className += ' responsive';
+    x.className += ' responsive';
     } else {
-      x.className = 'navtoggle';
+    x.className = 'navtoggle';
     }
 }
